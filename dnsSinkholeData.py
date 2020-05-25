721851ecc20596890f16da33f6379a6f1ae6a335
Spring School <truongblx26031999@gmail.com>
2020-05-25 15:03:30 +0700

    type your commit message here

--- dnsSinkholeData.py
+++ dnsSinkholeData.py
@@ -8,6 +8,8 @@ db = pymysql.connect("10.15.181.112","dnssinkhole","password","DomainServer" )
 # prepare a cursor object using cursor() method
 cursor = db.cursor()
 
+f = open("/etc/bind/named.conf.blacklists","w+")
+     
 # Prepare SQL query to INSERT a record into the database.
 sql = "SELECT * FROM domains"
 try:
@@ -16,12 +18,13 @@ try:
    # Fetch all the rows in a list of lists.
    results = cursor.fetchall()
    for row in results:
-      fname = row[0]
-
+      blackdomain = "zone \"" + row[0] + "\" {type master; file \"/etc/bind/client.nowhere\"; }; \r\n"
+      f.write(blackdomain)
       # Now print fetched result
-      print (fname)
+      print (blackdomain)
 except:
    print ("Error: unable to fetch data")
 
 # disconnect from server
+f.close()
 db.close()
